06e5569f78732dbe5bb51de5acd993d3f9d66964
pingram <ingrampaul411@gmail.com>
2025-12-10 19:21:28 +0000

    Publish occupancy grid

--- occupancy_grid.py
+++ occupancy_grid.py
@@ -1,15 +1,54 @@
+#!/usr/bin/env python3
+import rospy
 import cv2
 import numpy as np
-
-# Load grayscale map (0–255)
-img = cv2.imread("maps/project_map.pgm", cv2.IMREAD_GRAYSCALE)
-if img is None:
-    raise RuntimeError("Could not open maps/project_map.pgm")
+from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import Pose
 
 FREE_THRESHOLD = 254
 OCC_THRESHOLD = 205
 
-occ_grid = np.full(img.shape, -1, dtype=np.int8)
+def make_occupancy_grid():
+    # Load grayscale map (0–255)
+    img = cv2.imread("maps/project_map.pgm", cv2.IMREAD_GRAYSCALE)
+    if img is None:
+        raise RuntimeError("Could not open maps/project_map.pgm")
 
-occ_grid[img >= FREE_THRESHOLD] = 0
-occ_grid[img <= OCC_THRESHOLD] = 1
+    #Create occupancy grid
+    occ_grid = np.full(img.shape, -1, dtype=np.int8)
+    occ_grid[img >= FREE_THRESHOLD] = 0
+    occ_grid[img <= OCC_THRESHOLD] = 100
+
+    return occ_grid
+
+def publish_map():
+    rospy.init_node("map_publisher")
+
+    pub = rospy.Publisher("/map", OccupancyGrid, queue_size=1, latch=True)
+
+    grid = make_occupancy_grid()
+
+    msg = OccupancyGrid()
+    msg.header.frame_id = "map"
+
+    h, w = grid.shape
+    msg.info.width = w
+    msg.info.height = h
+    msg.info.resolution = 0.05  
+
+    # Origin: bottom-left of map in world coordinates
+    msg.info.origin = Pose()
+    msg.info.origin.position.x = 0.0
+    msg.info.origin.position.y = 0.0
+
+    # Flatten row-major for ROS
+    msg.data = grid.flatten().tolist()
+
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        msg.header.stamp = rospy.Time.now()
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == "__main__":
+    publish_map()
